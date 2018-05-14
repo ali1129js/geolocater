0e887a7b809e30b790ced68420888ee39fcf6494
ali1129js <ali1129js@gmail.com>
2018-05-14 09:16:01 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: Ali
  * @Date:   2018-05-13T17:37:04+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-13T19:01:27+02:00
+ * @Last modified time: 2018-05-14T09:14:49+02:00
  */
 
 import React, { Component } from 'react'
@@ -10,6 +10,7 @@ import logo from './logo.svg'
 import './App.css'
 
 const endpoint ='http://ip-api.com/json'
+const endpoint2 = 'https://api.openweathermap.org/data/2.5/weather?q='
 const API_KEY = '32941f4c12936fc51133cb69ca8f8b8a'
 const fetchOption = {
   method:'GET'
@@ -32,7 +33,7 @@ class App extends Component {
 }
 getWeather = async (e) => {
   e.preventDefault()
-  const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.yourLoc.city},${this.state.yourLoc.countryCode}&appid=${API_KEY}&units=metric`)
+  const api_call = await fetch(`${endpoint2}${this.state.yourLoc.city},${this.state.yourLoc.countryCode}&appid=${API_KEY}&units=metric`)
   const data = await api_call.json()
    if(data.main) {
     console.log(data)
@@ -62,7 +63,7 @@ getWeather = async (e) => {
         <div className="App">
           <header className="App-header" onClick={this.getWeather.bind(this)}>
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome from {this.state.yourLoc.city}</h1>
+            <h2 className="App-title">Welcome from {this.state.yourLoc.city}, {this.state.yourLoc.country}</h2>
           </header>
         </div>
       )
@@ -70,14 +71,10 @@ getWeather = async (e) => {
     const { temperature, description,city } = this.state
     return (
       <div className="App">
-        <header className="App-header" onClick={this.getWeather.bind(this)}>
+        <header className="App-header">
+          {city} today: {temperature}°C <i>{description}</i>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome from {this.state.yourLoc.city}</h1>
         </header>
-        <p className="App-intro">
-          The temperature in {city} is: {temperature}°C
-          The Conditions: <i> {description} </i>
-        </p>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/**
+ * @Author: Ali
+ * @Date:   2018-05-13T19:21:41+02:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-05-13T19:21:41+02:00
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
