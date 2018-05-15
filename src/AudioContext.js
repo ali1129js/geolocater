46c60e0aadc035914b8f5d5f8f19887890a1db5e
ali1129js <ali1129js@gmail.com>
2018-05-15 20:00:51 +0200

    ...

--- src/AudioContext.js
+++ src/AudioContext.js
@@ -2,14 +2,34 @@
  * @Author: Ali
  * @Date:   2018-05-14T09:30:46+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-05-14T21:22:36+02:00
+ * @Last modified time: 2018-05-15T19:58:45+02:00
  */
 import React, {Component} from 'react'
 
-const context = new AudioContext()
-const source = null
 
 class AudioContext extends Component {
+  constructor(){
+    super()
+    this.state = {
+      playing:false
+    }
+  }
+  handlePlayPress(){
+  if (!this.state.playing){
+    this.state({
+      playing:true
+    })
+  } else {
+  this.state({
+    playing:false
+  })
+  }
+  }
+  //web Audio initialization
+  componentDid
+  Mount(){
+    this.context = new AudioContext()
+    }
   render(){
     return (
       <h3> Hi from Theremin </h3>
